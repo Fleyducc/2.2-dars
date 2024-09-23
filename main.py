415dceeeee60d0dc759b0e8127fc2f7d0e9515eb
Fleyducc <167763678+Fleyducc@users.noreply.github.com>
2024-09-23 22:28:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+sonlar = []
+
+sonlar.append(1)
+sonlar.append(4)
+sonlar.append(77)
+sonlar.append(13)
+sonlar.append(99)
+
+print(sonlar)
+
+
+ism = ['Nodir', 'Ali', 'Vali', 'Alex', 'Xabibulloh']
+
+ismalexi = ism.index('Alex')
+
+print('Alex ismi:  ', ismalexi)
+
+davlat = ['Uzbekistan', 'Qirgizistan', 'Qazaqstan']
+
+davlat.insert(1, 'Tajikistan')
+
+print(davlat)
+
+raqam = [1, 2, 3, 4, 5]
+
+raqam.pop()
+
+print(raqam)
+
+ochirish = [1, 2, 3, 2, 4, 1, 2]
+
+ochirish.remove(2)
+ochirish.remove(2)
+ochirish.remove(2)
+
+print(ochirish)
+
+birlashgan = []
+
+birlashgan.append('Ilya')
+birlashgan.append('Gaz')
+birlashgan.append('Anna')
+birlashgan.append('Qozon')
+birlashgan.append('Vali')
+
+birlashgan.index('Anna')
+
+birlashgan.remove('Anna')
+
+birlashgan.append('Marlin')
+
+print(birlashgan)
